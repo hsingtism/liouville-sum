4f898bc2bc4829ac7f69bd182ecabbc0d604f01d
Hsingtism <hsing@flyingbat.net>
2021-11-21 22:59:47 -0500

    /= 3 and 6k+- 1 optimization

--- main.c
+++ main.c
@@ -9,11 +9,27 @@ long liouvFrag(unsigned long long seed) //since running count uses long
     int tz = __builtin_ctz(seed); //ctz returns int so
     pfCount += tz;
     seed = seed >> tz;
-    for (unsigned long divis = 3; divis <= sqrt(seed); divis = divis + 2) { //it's faster to call sqrt() than squre a 128uint 
-        while (seed % divis == 0) {
+    while (seed % 3 == 0)
+    {
+        pfCount++;
+        seed *= 0xAAAAAAAAAAAAAAAB; //divide by 3 hack
+    }
+
+    for (unsigned long divis = 6; divis <= sqrt(seed)+1; divis += 6)
+    { //it's faster to call sqrt() than squre a 128uint
+        divis--;
+        while (seed % divis == 0)
+        {
             pfCount++;
             seed /= divis;
         }
+        divis += 2;
+        while (seed % divis == 0)
+        {
+            pfCount++;
+            seed /= divis;
+        }
+        divis--;
     }
     if (seed > 2)
     {
@@ -32,8 +48,8 @@ int main()
         if (liouvSum == 0 || i % 10000000 == 0)
         {
             printf("%lu: %ld\n", i, liouvSum);
-            FILE * fp;
-            fp = fopen("computed.txt","a");
+            FILE *fp;
+            fp = fopen("computed.txt", "a");
             fprintf(fp, "%lu - %lu: %ld\n", (unsigned long)time(NULL), i, liouvSum);
             fclose(fp);
         }
