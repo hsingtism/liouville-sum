fd4d8a59dbe85b8bc0409f5d06f095b17140de8d
Hsing Lo <hsing@flyingbat.net>
2024-07-19 19:42:15 -0400

    computation in blocks of u64

--- entry.c
+++ entry.c
@@ -2,43 +2,56 @@
 #include <stdint.h>
 #include <math.h>
 
-#define TESTBLOCK 128
-
-uint32_t liouville_full(uint32_t n) {
+uint8_t liouville_full(uint64_t n) {
     if (n == 0) return 0;
     uint8_t factorCount = 0;
 
     uint16_t trailingZeroCount = __builtin_ctz(n);
-    factorCount += trailingZeroCount;
+    factorCount ^= trailingZeroCount & 1;
     n = n >> trailingZeroCount;
 
     while(n % 3 == 0) {
-        factorCount++;
+        factorCount ^= 1;
         n /= 3;
     }
     
     for(uint32_t div = 6; div <= (uint32_t)sqrt(n) + 1; div += 6) {
         while(n % (div - 1) == 0) {
-            factorCount++;
+            factorCount ^= 1;
             n /= div - 1;
         }
         while(n % (div + 1) == 0) {
-            factorCount++;
+            factorCount ^= 1;
             n /= div + 1;
         }
     }
  
     // case where the number is prime after the 2 and 3 test
-    factorCount += n > 2;
+    factorCount  ^= n > 2;
 
     return factorCount;
 }
 
+uint64_t liouvilleBlock(uint64_t starting) {
+    uint64_t block = 0;
+    for(uint64_t i = 0; i < 64; i++) {
+        block ^= (uint64_t)liouville_full(starting + i) << i;
+    }
+    
+    return block;
+}
+
 int main() {
 
-    for(uint32_t i = 1; i < TESTBLOCK; i++) {
-        printf("%u %u\n", i, liouville_full(i));
+    int32_t sum = -1; // start at -1 to account for liouville_full(0)
+    for(uint16_t i = 0; i < 10; i++) {
+        sum += __builtin_popcountll(liouvilleBlock(i * 64)) * -2 + 64;
+        printf("%u %ld\n", i * 64 + 63, sum);
     }
+
+    // for(uint32_t i = 1; i < 100000; i++) {
+    //     liouville_fullADD(i);
+    // }
 
     return 0;
 }
